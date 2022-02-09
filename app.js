c3a472a90e432db1eea6e9c8508a927b503caaff
Abby Kelly <akindred4@ivytech.edu>
2022-02-09 14:18:51 -0500

    added UserModel and updated user index file

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//code for app.js
+require("dotenv").config();
+const Express = require('express');
+const app = Express();
+const dbConnection = require('./db');
+
+app.use(require('./model/headers'))
+app.use(Express.json());
+
+const controllers = require('./controllers');
+
+dbConnection.authenticate()
+.then(() => dbConnection.sync())
+.then(() => {
+    app.listen(3001, () => {
+        console.log(`[Server]: App is listening on 3001.`);
+    });
+})
+
+.catch((err) => {
+    console.log(`[server]: Server crashed. Error = ${err}`);
+});
+
--- model/index.js
+++ model/index.js
@@ -1,1 +1,10 @@
-//code for index file
+const PieModel = require('./pie');
+const UserModel  = require('./user');
+
+
+
+module.exports = {
+  CORS: require('./headers'),
+  validateSession: require('./validate-session'),
+  PieModel, 
+  UserModel }
