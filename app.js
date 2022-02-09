f3ee889797b8e8329515df64e6e0756be00b10b5
Abby Kelly <akindred4@ivytech.edu>
2022-02-09 13:57:50 -0500

    updated ENV file

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const Express = require('express');
 const app = Express();
 const dbConnection = require('./db');
 
-app.use(require('./middleware/headers'))
+app.use(require('./model/headers'))
 app.use(Express.json());
 
 const controllers = require('./controllers');
+
+dbConnection.authenticate()
+.then(() => dbConnection.sync())
+.then(() => {
+    app.listen(3001, () => {
+        console.log(`[Server]: App is listening on 3001.`);
+    });
+})
+
+.catch((err) => {
+    console.log(`[server]: Server crashed. Error = ${err}`);
+});
